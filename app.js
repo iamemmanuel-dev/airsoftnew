8cfe22e0b19b12c9fb2a27755d174d8dff610b34
Ademola Emmanuel <emmanuelademola20@gmail.com>
2022-03-03 07:01:19 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,156 @@
+import data, { galleryData } from './assets/Data/data.js'
+const portfolio_row = document.querySelector('.portfolio_row')
+
+const goToSlide = count => {
+  document
+    .querySelectorAll('.imgSlide')
+    .forEach(
+      (slides, i) =>
+        (slides.style.transform = `translateX(${100 * (i - count)}%)`)
+    )
+}
+
+const activateDots = count => {
+  document
+    .querySelectorAll('.dots')
+    .forEach(dot => dot.classList.remove('dot-active'))
+
+  document
+    .querySelector(`.dots[data-slide='${count}']`)
+    .classList.add('dot-active')
+}
+
+window.addEventListener('load', () => {
+  /* NAVBAR LOGIC */
+  console.log(document.querySelector('.barsContainer'))
+  document.querySelector('.barsContainer').addEventListener('click', () => {
+    document
+      .querySelector('.navbar_listContainer')
+      .classList.toggle('toggleNav')
+  })
+
+  /* SLIDER LOGIC */
+  const imgSlides = document.querySelectorAll('.imgSlide')
+  let count = 0
+  let max_count = imgSlides.length - 1
+
+  const dotsContainer = document.querySelector('.dotsContainer')
+  imgSlides.forEach((_, i) => {
+    const html = `<div class='dots' data-slide=${i}></div>`
+    dotsContainer.insertAdjacentHTML('beforeend', html)
+  })
+
+  document.querySelectorAll('.dots').forEach(dot => {
+    dot.addEventListener('click', e => {
+      clearInterval(interval)
+      const { slide } = e.target.dataset
+      goToSlide(slide)
+      activateDots(slide)
+    })
+  })
+
+  activateDots(0)
+
+  const interval = setInterval(() => {
+    goToSlide(count)
+    activateDots(count)
+    count++
+    if (count > max_count) count = 0
+  }, 3000)
+
+  /* DISPLAY ABOUT LOGIC */
+  document
+    .querySelector('.about_btnContainer')
+    .addEventListener('click', ({ target }) => {
+      const id = target.dataset.id
+      if (target.classList.contains('aboutBtn')) {
+        document
+          .querySelectorAll('.aboutBtn')
+          .forEach(btn => btn.classList.remove('activeBtn'))
+
+        document
+          .querySelectorAll('.aboutArticle')
+          .forEach(article => article.classList.remove('activeArticle'))
+
+        document.querySelector('.aboutContent_btn').textContent = id
+
+        target.classList.add('activeBtn')
+        document.getElementById(id).classList.add('activeArticle')
+      }
+    })
+
+  /* PORTFOLIO DISPLAY LOGIC LOGIC */
+
+  portfolio_row.innerHTML = data
+    .map((items, index) => {
+      const { img, title, content } = items
+      return `
+         <article>
+          <div class="portfolio_col1">
+            <img src='${img}' alt=${title}/>
+          </div>
+          <div class="portfolio_col2">
+            <h3 class='tertiaryHeading'>${title}</h3>
+            <p>${content}</p>
+            <a href='#gallerySection' class='projectsLink' data-id=${index}>see projects &DownArrow;</a>
+          </div>
+         </article>`
+    })
+    .join('')
+
+  /* AIRSOFT PROJECTS DISPLAY LOGIC  */
+  const galleryRow = document.querySelector('.galleryBox_row')
+  const galleryBtns = document.querySelectorAll('.galleryBtn')
+  const gallerySubject = document.querySelector('.gallerySubject')
+  const Data = galleryData.filter(({ title }) => title === '3d art')
+
+  // Set default gallery subject
+  gallerySubject.textContent = Data[0].subject
+
+  //set default galleries
+  const displayGalleryColumns = data =>
+    data
+      .map(
+        items => `<div class="galleryImgContainer">
+        <img src="${items}" alt=${Data.title} />
+      </div>`
+      )
+      .join('')
+
+  galleryRow.innerHTML = displayGalleryColumns(Data[0].data)
+
+  galleryBtns.forEach(btn => {
+    const { id } = btn.dataset
+    btn.addEventListener('click', e => {
+      galleryBtns.forEach(btn => btn.classList.remove('activeGalleryBtn'))
+      e.target.classList.add('activeGalleryBtn')
+
+      const Data = galleryData.filter(({ title }) => title === id)
+      gallerySubject.textContent = Data[0].subject
+      galleryRow.innerHTML = displayGalleryColumns(Data[0].data)
+    })
+  })
+
+  /* SECTIONS INTO VIEW LOGIC */
+
+  const sections = document.querySelectorAll('section')
+
+  // const observer = new IntersectionObserver(
+  //   (entries, observer) => {
+  //     const [entry] = entries
+  //     if (!entry.isIntersecting) return
+  //     entry.target.classList.remove('hidden')
+  //     observer.unobserve(entry.target)
+  //   },
+  //   { root: null, threshold: 0.05 }
+  // )
+
+  // sections.forEach(section => {
+  //   section.classList.add('hidden')
+  //   observer.observe(section)
+  // })
+
+  /*GET FOOTER YEAR */
+
+  document.querySelector('.getYear').textContent = new Date().getFullYear()
+})
---
+++ assets/Data/data.js
@@ -0,0 +1,79 @@
+const airsoftData = [
+  {
+    id: 1,
+    img: './assets/images/illustration1.jpg',
+    title: '3D Realistic Art',
+    content: ` 
+    We create 3D realistic game art, caricaturing a real person, or simply inventing one. The unique and valuable quality we offer is our ability to give characters a sympathetic, organic, emotional look to make them look completely realistic.`,
+  },
+
+  {
+    id: 2,
+    img: './assets/images/Artboard 19.png',
+    title: '3D Cartoon Art',
+    content: `
+    We create 3D cartoon art for Children, we have a deep connection with Children's minds. We create eye-catching game characters that gradually lead to a flawless final design ready for production. `,
+  },
+
+  {
+    id: 3,
+    img: './assets/images/Artboard 1.png',
+    title: 'Children Book Illustrations',
+    content: `
+
+    We create illustrations that complement your style of writing and the message of your children’s book.
+    Style is quite a broad term. Our style ranges from cartoon-style or realistic, whimsical, stylized, watercolor, charcoal, etc.`,
+  },
+]
+
+export const galleryData = [
+  {
+    data: [
+      './assets/images/Artboard 5.png',
+      './assets/images/Artboard 9.png',
+      './assets/images/Artboard 12.png',
+      './assets/images/Artboard 14.png',
+      './assets/images/Artboard 15.png',
+      './assets/images/Artboard 17.png',
+      './assets/images/Artboard 18.png',
+      './assets/images/Artboard 20.png',
+      './assets/images/Artboard 21.png',
+    ],
+
+    title: '3d art',
+    subject: '3d Realistic Art',
+  },
+
+  {
+    data: [
+      './assets/images/Artboard 3.png',
+      './assets/images/Artboard 4.png',
+      './assets/images/Artboard 6.png',
+      './assets/images/Artboard 7.png',
+      './assets/images/Artboard 8.png',
+      './assets/images/Artboard 10.png',
+      './assets/images/Artboard 11.png',
+    ],
+
+    title: 'cartoon',
+    subject: 'CARTOON ART',
+  },
+
+  {
+    data: [
+      './assets/images/Artboard 1.png',
+      './assets/images/Artboard 2.png',
+      './assets/images/Artboard 13.png',
+      './assets/images/Artboard 22.png',
+      './assets/images/Artboard 23.png',
+      './assets/images/Artboard 24.png',
+      './assets/images/Artboard 25.png',
+      './assets/images/Artboard 26.png',
+      './assets/images/Artboard 27.png',
+    ],
+
+    title: 'children',
+    subject: 'CHILDREN BOOK ILLUSTRATION',
+  },
+]
+export default airsoftData
